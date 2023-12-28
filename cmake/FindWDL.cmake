55a4637baf3398f8ee577d13b9e2737dd31ba2a0
ak5k <42914711+ak5k@users.noreply.github.com>
2023-12-29 00:17:03 +0200

    Update WDL_INCLUDE_DIR path in FindWDL.cmake

--- cmake/FindWDL.cmake
+++ cmake/FindWDL.cmake
@@ -4,7 +4,7 @@ endif()
 
 find_path(WDL_INCLUDE_DIR
   NAMES WDL/wdltypes.h
-  PATHS ${CMAKE_SOURCE_DIR}/lib/WDL
+  PATHS ${CMAKE_SOURCE_DIR}/vendor/WDL
   NO_DEFAULT_PATH
 )
 mark_as_advanced(WDL_INCLUDE_DIR)
