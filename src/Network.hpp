a8944a3d03313b9d78e488a55897a6148e8c5689
ak5k <42914711+ak5k@users.noreply.github.com>
2024-01-03 03:14:40 +0200

    Remove unnecessary includes and commented code in Network.hpp

--- src/Network.hpp
+++ src/Network.hpp
@@ -1,7 +1,5 @@
-#include <algorithm>
 #include <unordered_map>
 #include <unordered_set>
-#include <vector>
 
 template <typename Node>
 class Network
@@ -16,37 +14,6 @@ public:
   {
     nodes[fromNode].insert(toNode);
   }
-
-  // void findAllPaths(Node startNode, Node endNode, std::vector<Node> path,
-  //                   std::vector<std::vector<Node>>& allPaths)
-  // {
-  //   path.push_back(startNode);
-
-  //   if (startNode == endNode)
-  //   {
-  //     allPaths.push_back(path);
-  //   }
-  //   else
-  //   {
-  //     for (Node node : nodes[startNode])
-  //     {
-  //       if (std::find(path.begin(), path.end(), node) == path.end())
-  //       {
-  //         findAllPaths(node, endNode, path, allPaths);
-  //       }
-  //     }
-  //   }
-
-  //   path.pop_back();
-  // }
-
-  // std::vector<std::vector<Node>> getAllPaths(Node startNode, Node endNode)
-  // {
-  //   std::vector<std::vector<Node>> allPaths;
-  //   std::vector<Node> path;
-  //   findAllPaths(startNode, endNode, path, allPaths);
-  //   return allPaths;
-  // }
 
   std::unordered_map<Node, std::unordered_set<Node>>& getNodes()
   {
