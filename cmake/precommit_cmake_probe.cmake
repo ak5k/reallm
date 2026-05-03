799bc35c35d053d75fbe94b26704d139a4803867
ak5k <42914711+ak5k@users.noreply.github.com>
2026-05-03 23:23:56 +0300

    chore: remove pre-commit verification probes

--- cmake/precommit_cmake_probe.cmake
+++
@@ -1,4 +0,0 @@
-set(PROBE_FILES one.cpp two.cpp)
-if(WIN32)
-    message(STATUS "probe")
-endif()
--- cmake/precommit_dummy.cmake
+++
@@ -1,4 +0,0 @@
-set(DUMMY_SOURCES x.cpp y.cpp z.cpp)
-if(WIN32)
-    message(STATUS "dummy2")
-endif()
